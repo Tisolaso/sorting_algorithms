b7113defb013d79a3fcabe63302e627b2ed85b60
Tisolaso <spanya2017@gmail.com>
2024-02-20 15:27:43 -0800

    lol

--- sort.h
+++ sort.h
@@ -27,7 +27,6 @@ void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
-void shell_sort(int *array, size_t size);
 void swap(int *a, int *b);
 size_t partition(int *array, size_t low, size_t high, size_t size);
 void quick_sort(int *array, size_t size);
